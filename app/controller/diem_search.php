0a49c26a01879f7037a0009e770b236dfc029b2d
martgold123 <73158024+martgold123@users.noreply.github.com>
2021-12-30 23:16:48 +0700

    Update diem_search.php

--- app/controller/diem_search.php
+++ app/controller/diem_search.php
@@ -14,19 +14,6 @@ isset($_GET['searchByTeacher'])){
     $data = $test->search($student,$subject,$teacher,$conn);
     extract($data);
 }
-
-
-if(isset($_GET['idD'])){
-    $id = $_GET['idD'];
-    $test->delete($id,$conn);
-    header('location: diem_search.php');
-}
-
-
-if(isset($_GET['idE'])){
-    header('location: diem_edit.php');
-}
-
 require_once 'C:/xampp/htdocs/web/quan_ly_diem_v1.0/app/view/diem_timkiem_input.php';
 
 ?>
